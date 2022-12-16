da04c4cba1f7b076cb976e291b666f3b198b3586
ullas kunder <ullaskunder3@gmail.com>
2022-12-16 13:03:11 +0530

    demo .exe

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+let box = document.getElementsByClassName('dock');
 const preview = document.querySelector('iframe');
 const loadingEle = document.getElementById('loading');
 const container = document.querySelector('.container');
@@ -12,8 +13,8 @@ function fetchFile() {
     reader.addEventListener("load", function () {
         // loadingEle.style.display = 'block';
         preview.style.opacity = 1;
-
         preview.src = reader.result;
+        document.onkeydown = keyPress;
     }, false);
 
     if (!(filename.length == 0)) {
@@ -26,10 +27,23 @@ function fetchFile() {
     }
 }
 function closeIframe() {
-    console.log("clicked");
     preview.src = "about:blank"
     container.style.display = "block"
     container.style.alignItems = "center";
     container.style.display = "flex";
     container.style.justifyContent = "center";
 }
+const keyPress = (e) => {
+    let eventObj = window.event ? event : e
+    if (eventObj.keyCode == 191 && eventObj.ctrlKey) {
+        console.log("class List",box[0].classList);
+        if (box[0].classList.contains('hidden')) {
+            box[0].classList.remove('hidden');
+            box[0].classList.remove('visuallyhidden');
+        } else {
+            box[0].classList.add('visuallyhidden');
+            box[0].classList.add('hidden');
+        }
+    }
+}
+document.onkeydown = keyPress;
